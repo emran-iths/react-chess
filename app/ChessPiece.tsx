33dec902aa659891652658c47733d10139922a1c
emran-iths <155668324+emran-iths@users.noreply.github.com>
2024-01-07 20:30:28 +0100

    added chess piece class and svg files for pieces

---
+++ app/ChessPiece.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image'
+import React, {
+    Component
+} from 'react'
+class ChessPiece extends Component {
+
+    render() {
+
+        let img_src = {
+            "w-knight": "Chess_nlt45",
+            "b-knight": "Chess_ndt45",
+            "w-pawn": "Chess_plt45",
+            "b-pawn": "Chess_pdt45",
+            "w-queen": "Chess_qlt45",
+            "b-queen": "Chess_qdt45",
+            "w-king": "Chess_klt45",
+            "b-king": "Chess_kdt45",
+            "w-bishop": "Chess_blt45",
+            "b-bishop": "Chess_bdt45",
+            "w-rook": "Chess_rlt45",
+            "b-rook": "Chess_rdt45",
+        } [this.props.type]
+        if (img_src === undefined)
+            img_src = 'No_image';
+
+        return < Image src = {
+            "/" + img_src + ".svg"
+        }
+        alt = "img"
+        width = {
+            46
+        }
+        height = {
+            46
+        }
+        />
+
+    }
+}
+
+
+export default ChessPiece
+
