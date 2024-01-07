6a6fea78a1b45c4ace16aa8c1f08cf7a343dfde8
emran-iths <155668324+emran-iths@users.noreply.github.com>
2024-01-07 21:19:39 +0100

    remove default css and replaced with css to show a chess board. added chess square

---
+++ app/ChessSquare.tsx
@@ -0,0 +1,29 @@
+import ChessPiece from './ChessPiece'
+import React, {
+    Component
+} from 'react'
+
+let letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
+let numbers = ["1", "2", "3", "4", "5", "6", "7", "8"];
+
+class ChessSquare extends Component {
+
+    isLight() {
+        return ((letters.indexOf(this.props.name[0]) % 2) +
+            (numbers.indexOf(this.props.name[1]) % 2)) % 2 == 0
+    }
+
+    render() {
+        return < td class = {
+                this.isLight() ? 'light' : 'dark'
+            } >
+            <
+            ChessPiece type = {
+                this.props.piece
+            }
+        />
+
+        </td> }
+}
+
+export default ChessSquare
